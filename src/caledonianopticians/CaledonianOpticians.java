6d6063373124d4f01e74ebe8b9b1532fd6d4323b
adamdon <adamdon89@gmail.com>
2018-02-20 11:05:09 +0000

    Commit main java files

--- src/caledonianopticians/CaledonianOpticians.java
+++ src/caledonianopticians/CaledonianOpticians.java
@@ -27,7 +27,7 @@ public class CaledonianOpticians extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Don't Say Caledonian Opticians");
+                System.out.println("Don't Say Caledonian Opticians 2");
             }
         });
         
