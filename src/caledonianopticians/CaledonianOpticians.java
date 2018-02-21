f14347d04689f14f2679d985f7b7198d0feefedc
adamdon <adamdon89@gmail.com>
2018-02-21 21:42:31 +0000

    Recoded scene layout from StackPane to GridPane with some test elements.

--- src/caledonianopticians/CaledonianOpticians.java
+++ src/caledonianopticians/CaledonianOpticians.java
@@ -1,51 +1,84 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package caledonianopticians;
 
+//import java.awt.Insets;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+
 
 /**
  *
- * @author admin
+ * @author adamdon <adamdon89@gmail.com>
  */
-public class CaledonianOpticians extends Application {
+public class CaledonianOpticians extends Application implements EventHandler<ActionEvent>
+{
+    Button btn;
+    Button btnSeach;
+    TextField userTextField;
+
+    
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say Caledonian Opticians");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Don't Say Caledonian Opticians5");
-            }
-        });
+    public void start(Stage primaryStage)
+    {
+        btn = new Button();
+        btn.setText("Caledonian Opticians");
+        btn.setOnAction(this);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        btnSeach = new Button();
+        btnSeach.setText("Seach Appointments");
+        btnSeach.setOnAction(this);
         
-        Scene scene = new Scene(root, 300, 250);
+        userTextField = new TextField();
+
         
-        primaryStage.setTitle("Hello World!");
+        
+        GridPane grid = new GridPane();
+        grid.add(btn, 0, 2);
+        grid.add(btnSeach, 0, 1);
+        grid.add(userTextField, 1, 1);
+       
+        
+        
+        
+
+        
+        Scene scene = new Scene(grid, 1280, 650);
         primaryStage.setScene(scene);
+        
+        grid.setAlignment(Pos.TOP_LEFT);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        Insets inset = new Insets(25, 25, 25, 25);
+        grid.setPadding(inset);
+        
+        
+        primaryStage.setTitle("Caledonian Opticians - Appointments System");
         primaryStage.show();
     }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
+    
+    public void handle(ActionEvent event)
+    {
+        if (event.getSource() == btn)
+        {
+            System.out.println("Test Print");
+        }
+        
+        if (event.getSource() == btnSeach)
+        {
+            System.out.println("Test Print - Search");
+        }
     }
     
 }
