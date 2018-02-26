c552e00f496179cdd70acac01e7b60289118b424
adamdon <adamdon89@gmail.com>
2018-02-26 22:11:50 +0000

    "User" class with constructors, getters and setters added. Basic Details used.

---
+++ src/caledonianopticians/User.java
@@ -0,0 +1,68 @@
+package caledonianopticians;
+
+public class User 
+{
+    private String srtFirstName;
+    private String srtLastName;
+    private int intReference;
+    private String srtAddress;
+    
+    User()
+    {
+        this.srtFirstName = "John";
+        this.srtLastName = "Doe";
+        this.intReference = 100000;
+        this.srtAddress = "1 Street Name, City, G51 1SW";
+    }
+    
+    User(String psrtFirstName, String psrtLastName, int pintReference, String psrtAddress)
+    {
+        this.srtFirstName = psrtFirstName;
+        this.srtLastName = psrtLastName;
+        this.intReference = pintReference;
+        this.srtAddress = psrtAddress;
+    }
+
+    public String getSrtFirstName()
+    {
+        return srtFirstName;
+    }
+
+    public void setSrtFirstName(String srtFirstName)
+    {
+        this.srtFirstName = srtFirstName;
+    }
+
+    public String getSrtLastName()
+    {
+        return srtLastName;
+    }
+
+    public void setSrtLastName(String srtLastName)
+    {
+        this.srtLastName = srtLastName;
+    }
+
+    public int getIntReference()
+    {
+        return intReference;
+    }
+
+    public void setIntReference(int intReference)
+    {
+        this.intReference = intReference;
+    }
+
+    public String getSrtAddress()
+    {
+        return srtAddress;
+    }
+
+    public void setSrtAddress(String srtAddress)
+    {
+        this.srtAddress = srtAddress;
+    }
+    
+    
+
+}
